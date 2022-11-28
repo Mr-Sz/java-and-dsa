92bdbea18f1ee12ef421842eeaf3e77eaf96946f
“Mr-Sz” <shahroz14k@gmail.com>
2022-11-28 22:01:54 +0530

    first java program

---
+++ testfolder/first program/src/fever_verifier/Main.java
@@ -0,0 +1,9 @@
+package fever_verifier;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("this is the main function in main class");
+        float fever_temp=99.5f; //in fahrenheit
+    }
+}
